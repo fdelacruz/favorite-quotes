05d16451511f1a679bfaff53b94a29111f948a96
Francisco De La Cruz <helloquico@gmail.com>
2016-01-06 13:19:16 -0500

    Test the Redux Store - Initial State

---
+++ src/state/store.spec.js
@@ -0,0 +1,17 @@
+import {store} from './store';
+import expect from 'expect';
+
+describe('store', () => {
+
+  it('should initialize', () => {
+    const actual = store.getState();
+    const expected = {
+      quotes: [],
+      theme: {
+        color: '#5DC4C6'
+      }
+    };
+
+    expect(actual).toEqual(expected);
+  });
+});
