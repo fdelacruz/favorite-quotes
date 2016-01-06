cf21ead2a584afb212d00feb0cf7f2183f8ebd51
Francisco De La Cruz <helloquico@gmail.com>
2016-01-06 16:21:34 -0500

    Test Redux Reducers

---
+++ src/home/quote/state/quoteReducer.spec.js
@@ -0,0 +1,67 @@
+import expect from 'expect';
+import quoteReducer from './quoteReducer';
+
+describe('quoteReducer', () => {
+
+  function stateBefore() {
+    return [
+      {
+        text: 'Lorem ipsum',
+        author: 'Jane Doe',
+        id: 1,
+        likeCount: 7
+      },
+      {
+        text: 'Ullamco laboris nisi ut aliquip',
+        author: 'John Smith',
+        id: 2,
+        likeCount: 0
+      }
+    ];
+  }
+
+  it('should add quotes by id', () => {
+    const action = {
+      type: 'ADD_QUOTE_BY_ID',
+      payload: {
+        text: 'This is a new quote',
+        author: 'Someone awesome',
+        id: 3,
+        likeCount: 0
+      }
+    };
+    const actual = quoteReducer(stateBefore(), action);
+    const expected = [
+      {
+        text: 'Lorem ipsum',
+        author: 'Jane Doe',
+        id: 1,
+        likeCount: 7
+      },
+      {
+        text: 'Ullamco laboris nisi ut aliquip',
+        author: 'John Smith',
+        id: 2,
+        likeCount: 0
+      },
+      {
+        text: 'This is a new quote',
+        author: 'Someone awesome',
+        id: 3,
+        likeCount: 0
+      }
+
+    ];
+    expect(actual).toEqual(expected);
+  });
+
+  it('should return previous state when trying to make a quote like count negative', () => {
+    const action = {
+      type: 'UNLIKE_QUOTE_BY_ID',
+      payload: {id: 0}
+    };
+    const actual = quoteReducer(stateBefore(), action);
+    const expected = stateBefore();
+    expect(actual).toEqual(expected);
+  });
+});
