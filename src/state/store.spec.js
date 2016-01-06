9bd393eceb9e69f47275b10d5f58c2e182284c51
Francisco De La Cruz <helloquico@gmail.com>
2016-01-06 12:52:16 -0500

    Test the Redux Store - Multiple Actions

--- src/state/store.spec.js
+++ src/state/store.spec.js
@@ -3,12 +3,62 @@ import expect from 'expect';
 
 describe('store', () => {
 
-  it('should initialize', () => {
+  it('should work with a series of actions', () => {
+    const actions = [
+      {
+        type: 'ADD_QUOTE_BY_ID',
+        payload: {
+          text: 'The best way to cheer yourself up is to try to cheer somebody else up',
+          author: 'Mark Twain',
+          id: 1,
+          LikeCount: 24
+        }
+      },
+      {
+        type: 'ADD_QUOTE_BY_ID',
+        payload: {
+          text: 'Whatever you are, be a good one.',
+          author: 'Abraham Lincoln',
+          id: 2,
+          likeCount: 0
+        }
+      },
+      {
+        type: 'REMOVE_QUOTE_BY_ID',
+        payload: {id: 1}
+      },
+      {
+        type: 'LIKE_QUOTE_BY_ID',
+        payload: {id: 2}
+      },
+      {
+        type: 'LIKE_QUOTE_BY_ID',
+        payload: {id: 2}
+      },
+      {
+        type: 'UNLIKE_QUOTE_BY_ID',
+        payload: {id: 2}
+      },
+      {
+        type: 'UPDATE_THEME_COLOR',
+        payload: {color: '#777777'}
+      }
+    ];
+
+    actions.forEach(action => store.dispatch(action));
+
     const actual = store.getState();
     const expected = {
-      quotes: [],
+      quotes: [
+        {
+          text: 'Whatever you are, be a good one.',
+          author: 'Abraham Lincoln',
+          id: 2,
+          likeCount: 1
+        }
+      ],
       theme: {
-        color: '#5DC4C6'
+        color: '#777777'
       }
     };
 
